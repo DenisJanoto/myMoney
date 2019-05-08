ecd9b66d2c533d7487984235f757d54dba22f21d
Denis Janoto <denisjanoto@MacBook-Air-de-Denis.local>
2019-05-08 13:27:14 -0300

    Initial Commit

---
+++ myMoney/FireBaseReturnData.swift
@@ -0,0 +1,9 @@
+//
+//  FireBaseReturnData.swift
+//  myMoney
+//
+//  Created by Denis Janoto on 08/04/2019.
+//  Copyright © 2019 Denis Janoto. All rights reserved.
+//
+
+import Foundation
---
+++ myMoney/FirebaseAccess.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseAccess.swift
+//  myMoney
+//
+//  Created by Denis Janoto on 07/04/2019.
+//  Copyright © 2019 Denis Janoto. All rights reserved.
+//
+
+import Foundation
